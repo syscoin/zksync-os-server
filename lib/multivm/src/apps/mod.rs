03bef27b4b3d39d89acb40c6be4b777b22a9aec5
perekopskiy <mikeson.dp@gmail.com>
2025-12-04 18:41:52 +0200

    use zkos v0.2.5 in-place

--- lib/multivm/src/apps/mod.rs
+++ lib/multivm/src/apps/mod.rs
@@ -80,7 +80,7 @@ pub mod v5 {
     use std::sync::OnceLock;
 
     pub const SINGLEBLOCK_BATCH_APP: &[u8] = include_bytes!(concat!(
-        env!("ZKSYNC_OS_0_2_4_SOURCE_PATH"),
+        env!("ZKSYNC_OS_0_2_5_SOURCE_PATH"),
         "/singleblock_batch.bin"
     ));
 
@@ -104,7 +104,7 @@ pub mod v5 {
     }
 
     pub const SINGLEBLOCK_BATCH_LOGGING_ENABLED: &[u8] = include_bytes!(concat!(
-        env!("ZKSYNC_OS_0_2_4_SOURCE_PATH"),
+        env!("ZKSYNC_OS_0_2_5_SOURCE_PATH"),
         "/singleblock_batch_logging_enabled.bin"
     ));
 
@@ -128,7 +128,7 @@ pub mod v5 {
     }
 
     pub const MULTIBLOCK_BATCH: &[u8] = include_bytes!(concat!(
-        env!("ZKSYNC_OS_0_2_4_SOURCE_PATH"),
+        env!("ZKSYNC_OS_0_2_5_SOURCE_PATH"),
         "/multiblock_batch.bin"
     ));
 
