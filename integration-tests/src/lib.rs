3acd36800e5ef740c4f9009c089a1b2eedb40631
perekopskiy <mikeson.dp@gmail.com>
2026-03-18 19:46:54 +0100

    use updated prover

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -958,7 +958,7 @@ impl AnvilL1 {
 fn prover_release_for_protocol(protocol_version: &str) -> &'static str {
     match protocol_version {
         PROTOCOL_VERSION => "v0.7.1",
-        PROTOCOL_VERSION_V31_0 => "dev-20260317-v7",
+        PROTOCOL_VERSION_V31_0 => "dev-20260318-v7",
         _ => {
             panic!("unsupported protocol version `{protocol_version}` for prover binary selection")
         }
